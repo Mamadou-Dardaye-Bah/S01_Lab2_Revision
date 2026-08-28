41cd411e9782e4949bb239e2a9f6de48e1f723b5
Mamadou Dardaye Bah <2393818@cegepmontpetit.ca>
2026-08-28 10:04:23 -0400

    FCT ZombieType Create et Validation
    Ajout de la validation côté client et côté serveur pour ZombieType.

--- ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -22,5 +22,16 @@ namespace ZombieParty.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Create(Models.ZombieType zombieType)
+        {
+            if (ModelState.IsValid)
+            {
+                // Ajouter à la BD
+            }
+
+            return this.View(zombieType);
+        }
+
     }
 }
--- ZombieParty/ZombieParty/Models/ZombieType.cs
+++ ZombieParty/ZombieParty/Models/ZombieType.cs
@@ -1,9 +1,14 @@
 ﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace ZombieParty.Models
 {
     public class ZombieType
     {
         public int Id;
+        [Required(ErrorMessage ="Type Name has to be filled.")]
+        [Display(Name = "Type Name")]
         public string TypeName;
     }
 }
